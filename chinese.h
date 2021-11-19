52f53f8a55bf0d924cb4560bcd52ea34eedc1985
mizu1 <57716668+mizu1@users.noreply.github.com>
2021-11-19 23:18:48 +0800

    Add files via upload

---
+++ chinese.h
@@ -0,0 +1,15 @@
+#define 若 if
+#define 否则 else
+#define 循环 while 
+#define 做 do
+#define 迭代 for  
+#define 输入 scanf 
+#define 输出 printf 
+#define 主函数 main
+#define 返回 return
+#define 无效的 void
+#define 整型 int
+#define 浮点型 float
+#define 双浮点 double
+#define 长整型 long int
+#define 结构体 struct
